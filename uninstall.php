cde9c8a6b96aad42024984c78b6c44f4fa2b888d
Andre de Almeida <andre@metaweb.marketing>
2022-06-13 12:05:07 -0700

    JS Token check re-added

--- uninstall.php
+++ uninstall.php
@@ -1,7 +1,7 @@
 <?php // exit if uninstall constant is not defined
 if (!defined('WP_UNINSTALL_PLUGIN')) exit;
 //remove power_bi specifically created options
-//$options = ['power_bi_settings', '_powerbi_embed_flushed', 'power_bi_credentials','power_bi_management_azure_credentials','page_on_front'];
+//$options = ['power_bi_settings', '_powerbi_embed_flushed', 'power_bi_credentials','power_bi_management_azure_credentials'];
 $options = ['_powerbi_embed_flushed'];
 foreach($options as $option){
     if(get_option($option)) delete_option($option);
